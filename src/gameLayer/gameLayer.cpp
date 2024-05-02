e68f65a60248c46031d69b13b634664ccd95f387
JMH2323 <johnmasonhaley23@gmail.com>
2024-05-02 14:57:47 -0400

    Draw player and Move

--- src/gameLayer/gameLayer.cpp
+++ src/gameLayer/gameLayer.cpp
@@ -11,9 +11,20 @@
 #include <gl2d/gl2d.h>
 #include <platformTools.h>
 
+struct GameplayData
+{
+	glm::vec2 playerPos = { 100,100 };
+};
 
+GameplayData data;
+
+#pragma region textureLoading
 
 gl2d::Renderer2D renderer;
+
+gl2d::Texture playerTexture;
+
+#pragma endregion
 
 bool initGame()
 {
@@ -21,7 +32,7 @@ bool initGame()
 	gl2d::init();
 	renderer.create();
 
-	
+	playerTexture.loadFromFile(RESOURCES_PATH "spaceShip/ships/green.png", true);
 	
 	return true;
 }
@@ -42,8 +53,58 @@ bool gameLogic(float deltaTime)
 #pragma endregion
 
 
+#pragma region movement
 
-	renderer.renderRectangle({100,100, 100, 100}, Colors_Blue);
+	glm::vec2 move = {};
+
+
+	// Movement through Vectors
+	// Move Up
+	if (
+		platform::isButtonHeld(platform::Button::W) ||
+		platform::isButtonHeld(platform::Button::Up)
+		)
+	{
+		move.y = -1;
+	}
+	// Move Down
+	if (
+		platform::isButtonHeld(platform::Button::S) ||
+		platform::isButtonHeld(platform::Button::Down)
+		)
+	{
+		move.y = 1;
+	}
+	// Move Left
+	if (
+		platform::isButtonHeld(platform::Button::A) ||
+		platform::isButtonHeld(platform::Button::Left)
+		)
+	{
+		move.x = -1;
+	}
+	// Move Right
+	if (
+		platform::isButtonHeld(platform::Button::D) ||
+		platform::isButtonHeld(platform::Button::Right)
+		)
+	{
+		move.x = 1;
+	}
+
+	// Normalize Movement
+	if (move.x != 0 || move.y != 0)
+	{
+		move = glm::normalize(move);
+		move *= deltaTime * 200; // 200 pixels per second
+		data.playerPos += move;
+	}
+
+
+#pragma endregion
+
+
+	renderer.renderRectangle({ data.playerPos, 200, 200 }, playerTexture);
 
 
 	renderer.flush();
